c1a61f69d1cd9f982679eec0ceacad3489311182
Ulku <ulkuislamoglu@gmail.com>
2020-02-12 13:09:11 -0600

    first commit Ulku

--- src/test/java/pages/BillsOfMaterials.java
+++ src/test/java/pages/BillsOfMaterials.java
@@ -3,5 +3,6 @@ package pages;
 public class BillsOfMaterials {
     public static void main(String[] args) {
         System.out.println("Ulku Islamoglu");
+        System.out.println("Trying");
     }
 }
